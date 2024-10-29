0ec3f8f474fd286b235fbc6dd2290f80a92674b9
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-10-29 07:58:39 -0300

    Se crea la clase usuario

---
+++ TotosBackery/capaEntidades/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidades
+{
+    internal class Usuario
+    {
+        private int id;
+        private static int idCont = 0;
+        private string nombre;
+        private string apellido;
+        private string user;
+        private string contraseña;
+        private bool admin;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string User { get => user; set => user = value; }
+        public string Contraseña { get => contraseña; set => contraseña = value; }
+        public bool Admin { get => admin; set => admin = value; }
+
+        public Usuario(string nombre, string apellido, string user, string contraseña, bool admin)
+        {
+            this.id = ++idCont;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.user = user;
+            this.contraseña = contraseña;
+            this.admin = admin;
+        }
+    }
+}
--- TotosBackery/capaEntidades/obj/Debug/net8.0/capaEntidades.AssemblyInfo.cs
+++ TotosBackery/capaEntidades/obj/Debug/net8.0/capaEntidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5154b6ab71b8ca1b134ebb2ac78e9ab2f5da41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2afea8b9dcbf1473b714b12de86da57ed3c9621d")]
 [assembly: System.Reflection.AssemblyProductAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
