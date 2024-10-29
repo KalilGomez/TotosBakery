2d5154b6ab71b8ca1b134ebb2ac78e9ab2f5da41
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-10-29 07:52:28 -0300

    #

---
+++ TotosBackery/capaEntidades/Cliente.cs
@@ -0,0 +1,29 @@
+﻿namespace capaEntidades
+{
+    public class Cliente
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private string telefono;
+        private string mail;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Mail { get => mail; set => mail = value; }
+
+        public Cliente(int id, string nombre, string apellido, string direccion, string telefono, string mail)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.mail = mail;
+        }
+    }
+}
