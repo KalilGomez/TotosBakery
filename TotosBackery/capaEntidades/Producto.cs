2afea8b9dcbf1473b714b12de86da57ed3c9621d
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-10-29 07:57:57 -0300

    Se crea objeto producto

---
+++ TotosBackery/capaEntidades/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaEntidades
+{
+    internal class Producto
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private double precio;
+        private int cantidad;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+        public Producto(int id, string nombre, string descripcion, double precio, int cantidad)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.cantidad = cantidad;
+        }
+    }
+}
--- TotosBackery/capaEntidades/obj/Debug/net8.0/capaEntidades.AssemblyInfo.cs
+++ TotosBackery/capaEntidades/obj/Debug/net8.0/capaEntidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b33e9202989bf6570198b462dec8a88ac69940c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5154b6ab71b8ca1b134ebb2ac78e9ab2f5da41")]
 [assembly: System.Reflection.AssemblyProductAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
