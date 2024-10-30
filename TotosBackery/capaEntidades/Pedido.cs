ad127c251cf467d29e743a28d69821affd24fd87
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-10-29 21:03:42 -0300

    Se crea objeto pedido

--- TotosBackery/capaEntidades/Pedido.cs
+++ TotosBackery/capaEntidades/Pedido.cs
@@ -8,6 +8,24 @@ namespace capaEntidades
 {
     public class Pedido
     {
+        private int id;
+        private string estado;
+        private string met_pago;
+        private DateTime fecha;
+        private string direccion;
 
+        public int Id { get => id; set => id = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Met_pago { get => met_pago; set => met_pago = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public Pedido(int id, string estado, string met_pago, DateTime fecha, string direccion)
+        {
+            this.id = id;
+            this.estado = estado;
+            this.met_pago = met_pago;
+            this.fecha = fecha;
+            this.direccion = direccion;
+        }
     }
 }
