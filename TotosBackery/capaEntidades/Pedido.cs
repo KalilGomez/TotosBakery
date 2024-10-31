908372e35dae78ff64ac3dafee286ae405c14977
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-10-31 20:19:07 -0300

    Se modifica objeto pedido

--- TotosBackery/capaEntidades/Pedido.cs
+++ TotosBackery/capaEntidades/Pedido.cs
@@ -13,12 +13,15 @@ namespace capaEntidades
         private string met_pago;
         private DateTime fecha;
         private string direccion;
+        private Cliente oCliente;
 
         public int Id { get => id; set => id = value; }
         public string Estado { get => estado; set => estado = value; }
         public string Met_pago { get => met_pago; set => met_pago = value; }
         public DateTime Fecha { get => fecha; set => fecha = value; }
         public string Direccion { get => direccion; set => direccion = value; }
+        public Cliente OCliente { get => oCliente; set => oCliente = value; }
+
         public Pedido(int id, string estado, string met_pago, DateTime fecha, string direccion)
         {
             this.id = id;
