83080a160d8fd3f191e15cd04e68ab576a6f2370
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-11-17 17:26:09 -0300

    #

--- TotosBackery/capaPresentacion/obj/Debug/net8.0-windows/capaPresentacion.AssemblyInfo.cs
+++ TotosBackery/capaPresentacion/obj/Debug/net8.0-windows/capaPresentacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capaPresentacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be5c89bb547a0b406ddaf426c51e292cdec58705")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c190050c96ccb699c52622a13cc8ca61145a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("capaPresentacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capaPresentacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
