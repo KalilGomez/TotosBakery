6206f22675906957d655debb7e52b282c5d160c0
Kalil Gomez <Kalil_Gomez@hotmail.com>
2024-11-18 21:25:44 -0300

    #

--- TotosBackery/capaEntidades/obj/Debug/net8.0/capaEntidades.AssemblyInfo.cs
+++ TotosBackery/capaEntidades/obj/Debug/net8.0/capaEntidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c2590dfc762d21d1344bad09eb051aae31d2d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e547f985e20e60ce3d925c657551212aa4bc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capaEntidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TotosBackery/capaNegocio/obj/Debug/net8.0/capaNegocio.AssemblyInfo.cs
+++ TotosBackery/capaNegocio/obj/Debug/net8.0/capaNegocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("capaNegocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c2590dfc762d21d1344bad09eb051aae31d2d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81e547f985e20e60ce3d925c657551212aa4bc68")]
 [assembly: System.Reflection.AssemblyProductAttribute("capaNegocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("capaNegocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
